c5e0af68b6f33fd71b9f7cdae1863cc48c69895d
Jonas <r0831281@student.thomasmore.be>
2023-10-28 00:26:19 +0200

    fixes

--- ap_front/lib/pages/shared/bottomnav.dart
+++ ap_front/lib/pages/shared/bottomnav.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
-
 //this is the bottom navigation bar, consisting of 4 icons each leading to a different page
-
 
 class MyBottomNavigation extends StatelessWidget {
   const MyBottomNavigation({Key? key}) : super(key: key);
@@ -18,17 +16,6 @@ class MyBottomNavigation extends StatelessWidget {
           ),
           IconButton(
             onPressed: () => Navigator.of(context).pushNamed('/camera'),
-
-        child: Row(children: [
-          IconButton(
-            icon: const Icon(Icons.stars_outlined),
-            onPressed: //navigate to reviewPage
-                () => Navigator.of(context).pushNamed('/review'),
-          ),
-          IconButton(
-            onPressed: //navigate to camera Page
-                () => Navigator.of(context).pushNamed('/camera'),
-
             icon: const Icon(Icons.camera),
           ),
           //open camera button
@@ -41,5 +28,7 @@ class MyBottomNavigation extends StatelessWidget {
             onPressed: () => Navigator.of(context).pushNamed('/review'),
           ),
         ]));
+
+    //just a comment
   }
 }
