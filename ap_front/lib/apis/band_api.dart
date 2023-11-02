f4d2fe5d2ee5b9e9364e4bf1329bb004150832af
MaartenWilloqué <r0237151@student.thomasmore.be>
2023-11-02 11:23:56 +0100

    api gateway

--- ap_front/lib/apis/band_api.dart
+++ ap_front/lib/apis/band_api.dart
@@ -3,10 +3,10 @@ import 'package:ap_front/models/bandWithAlbum.dart';
 import 'package:http/http.dart' as http;
 
 class BandApi {
-  static String server = 'album-service-maartenwilloque.cloud.okteto.net';
+  static String server = 'api-gateway-maartenwilloque.cloud.okteto.net';
 
   static Future<BandWithAlbum> fetchBandWithAlbums(String bandId) async {
-    var url = Uri.https(server, '/api/band/$bandId');
+    var url = Uri.https(server, '/band/$bandId');
     final response = await http.get(url);
     if (response.statusCode == 200) {
       BandWithAlbum band = json.decode(response.body);
