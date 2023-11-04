2965987b63443c0225fabcaac2284b3c5e7c08fc
Dries <driesvandoorne@hotmail.com>
2023-11-04 16:15:51 +0100

    start

--- ap_front/lib/pages/review.dart
+++ ap_front/lib/pages/review.dart
@@ -1,4 +1,7 @@
 import 'package:ap_front/models/ratingWithAlbum.dart';
+import 'package:ap_front/textstyles/loadingstyles.dart';
+import 'package:ap_front/widgets/ratingstars.dart';
+import 'package:ap_front/widgets/titledisplay.dart';
 import 'package:flutter/material.dart';
 import 'package:ap_front/pages/shared/bottomnav.dart';
 import 'package:ap_front/apis/rating_api.dart';
@@ -15,9 +18,11 @@ class _ReviewPageState extends State<ReviewPage> {
   List<RatingWithAlbum> _ratingList = [];
   bool loading = true;
   String userId = "";
+  String userName = "";
 
   String getID() {
     userId = LocalStorage('user_storage').getItem('guid');
+    userName = LocalStorage('user_storage').getItem('username');
     return userId;
   }
 
@@ -42,30 +47,63 @@ class _ReviewPageState extends State<ReviewPage> {
 
   @override
   Widget build(BuildContext context) {
+    TextTheme theme = Theme.of(context).textTheme;
+    ColorScheme scheme = Theme.of(context).colorScheme;
+    const double heightBetweenElements = 15;
+
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const SizedBox(height: 30),
-            if (loading)
-              const CircularProgressIndicator()
-            else if (_ratingList.isEmpty)
-              const Text("No ratings found")
-            else
-              Expanded(
-                child: ListView.builder(
-                  itemCount: _ratingList.length,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      title: Text(_ratingList[index].album.title),
-                      subtitle: Text(_ratingList[index].score.toString()),
-                    );
-                  },
-                ),
-              ),
-          ],
+      appBar: AppBar(
+        title: Text("Review Page", style: theme.displayLarge),
+        backgroundColor: scheme.primary,
+        leadingWidth: 100,
+        toolbarHeight: 75,
+        iconTheme: const IconThemeData(
+          color: Colors.white,
         ),
+        centerTitle: true,
+      ),
+      body: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          const SizedBox(
+            height: heightBetweenElements,
+          ),
+          TitleDisplayWidget(
+            title: "Reviews by $userName",
+          ),
+          const SizedBox(
+            height: heightBetweenElements,
+          ),
+          loading
+              ? const Center(
+                  child: Text(
+                    "loading ratings...",
+                    style: loadingHeadline,
+                  ),
+                )
+              : _ratingList.isEmpty
+                  ? const Text("No ratings found")
+                  : Expanded(
+                      child: ListView.builder(
+                        itemCount: _ratingList.length,
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            title: Column(children: <Widget>[
+                              Text(
+                                _ratingList[index].album.title,
+                                style: theme.headlineSmall,
+                              ),
+                              RatingStars(
+                                rating: _ratingList[index].score.toDouble(),
+                                starSize: 25.0,
+                              ),
+                            ]),
+                          );
+                        },
+                      ),
+                    ),
+        ],
       ),
       bottomNavigationBar: const MyBottomNavigation(),
     );
