1d9c6cae67bd096fff8061d5c2ca6232e82d04cf
MaartenWilloqué <r0237151@student.thomasmore.be>
2023-11-04 01:12:29 +0100

    rating api added

--- ap_front/lib/apis/rating_api.dart
+++ ap_front/lib/apis/rating_api.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
+import 'dart:ffi';
 
 import 'package:ap_front/models/averageRating.dart';
 import 'package:ap_front/models/rating.dart';
+import 'package:ap_front/models/ratingWithAlbum.dart';
 import 'package:http/http.dart' as http;
 
 class RatingApi {
@@ -32,6 +34,32 @@ class RatingApi {
       return AverageRating.fromJson(jsonDecode(response.body));
     } else {
       throw Exception('Failed to get rating');
+    }
+  }
+
+  static Future<String> deleteRating(Long id) async {
+    var url = Uri.https(server, '/rating/$id');
+    final http.Response response = await http.delete(url);
+    if (response.statusCode == 200) {
+      return "deleted";
+    } else {
+      throw Exception('Failed to get rating');
+    }
+  }
+
+  static Future<List<RatingWithAlbum>> getRatingsWithAlbum(
+      String userId) async {
+    var url = Uri.https(server, '/ratings/$userId');
+
+    final http.Response response = await http.get(url);
+    if (response.statusCode == 200) {
+      List<dynamic> ratingsJson = json.decode(response.body);
+      List<RatingWithAlbum> ratings = ratingsJson
+          .map((dynamic item) => RatingWithAlbum.fromJson(item))
+          .toList();
+      return ratings;
+    } else {
+      throw Exception('Failed to load ratings');
     }
   }
 }
